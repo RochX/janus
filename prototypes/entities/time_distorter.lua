9b94243f892d542d41d5618d5f3cc4bd46f2a161
RochX <xavierwsilva+git@gmail.com>
2025-02-06 22:57:19 -0500

    graphics: Add more pleasing time distorter temp graphic; thanks FrDae

--- prototypes/entities/time_distorter.lua
+++ prototypes/entities/time_distorter.lua
@@ -5,14 +5,13 @@ local time_distorter_entity = {
   name="janus-time-distorter",
   subgroup="janus-time-distorter-subgroup",
   icon=helper.sprite "time_distorter.png",
-  icon_size=64,
+  icon_size=200,
   crafting_speed=1,
   crafting_categories={"janus-shiftite"},
   energy_usage = "1MW",
   energy_source = {type = "electric", usage_priority = "secondary-input"},
 
   minable={mining_time=0.5, result="janus-time-distorter"},
-
   collision_box = {{-2.4, -2.4}, {2.4, 2.4}},
   selection_box = {{-2.5, -2.5}, {2.5, 2.5}},
   drawing_box = {{-2.5, -2.5}, {2.5, 2.5}},
@@ -39,13 +38,13 @@ local time_distorter_entity = {
         {
           filename = helper.sprite "time_distorter.png",
           frame_count = 1,
-          height = 64,
+          height = 200,
           line_length = 8,
           priority = "high",
           -- TODO figure out exactly how this scaling works;
           -- 2.5 fits 64x64px image in 5x5 tiles in game
-          scale=2.5,
-          width = 64
+          scale=0.8,
+          width = 200
         }
       }
     }
